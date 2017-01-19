0c6ca5c3d43dad44bc1bdb1273c930a16b04ba7f
Setsuodu <339131001@qq.com>
2017-01-19 17:15:28 +0800

    first version
    PC Android test.
    Abs with material, texture dependencies.

---
+++ Assets/BundleLoader.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BundleLoader : MonoBehaviour {
+
+    public string bundleURL = "http://www.setsuodu.com/ABs/pikachu";
+    public string assetName = "pikachu";
+    AssetBundle bundle;
+
+    #region LoadFromCacheOrDownload下载或缓存方法
+    void Start()
+    {
+        StartCoroutine(LoadBD());
+    }
+
+    private IEnumerator LoadBD()
+    {
+        WWW download = WWW.LoadFromCacheOrDownload(bundleURL, 0);
+        yield return download;
+
+        if (download.error != null)
+        {
+            Debug.Log(download.error);
+        }
+        else
+        {
+            if (!download.isDone)
+            {
+                Debug.Log(download.progress);
+            }
+            else
+            {
+                AssetBundle bundle = download.assetBundle;
+                GameObject go = Instantiate(bundle.LoadAsset(assetName)) as GameObject;
+            }
+        }
+    }
+    #endregion
+
+    #region WWW直接下载方法
+    /*
+    IEnumerator Start()
+    {
+        WWW www = new WWW(bundleURL);
+        yield return www;
+        if (www.error != null)
+        {
+            throw new System.Exception("There an error : " + www.error);
+        }
+        else
+        {
+            bundle = www.assetBundle;
+        }
+        bundleOBJ(wwwGameObject);
+    }
+
+    public void Spawn()
+    {
+        if (assetName == "")
+        {
+            Instantiate(bundle.mainAsset);
+        }
+        else
+        {
+            Instantiate(bundle.LoadAsset(assetName));
+        }
+    }
+
+    public GameObject wwwGameObject;
+    GameObject bundleOBJ(GameObject go)
+    {
+        if (assetName == "")
+        {
+            go = Instantiate(bundle.mainAsset) as GameObject;
+        }
+        else
+        {
+            go = Instantiate(bundle.LoadAsset(assetName)) as GameObject;
+        }
+        return go;
+    }
+    */
+    #endregion
+}
---
+++ Assets/Editor/CreateAssetBundle.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CreateAssetBundle : Editor
+{
+    [MenuItem("Example/Build Android")]
+    static void BuildAll()
+    {
+        string outputPath = Application.dataPath + "/ABs";
+        if (!Directory.Exists(outputPath))
+        {
+            Directory.CreateDirectory(outputPath);
+        }
+        BuildPipeline.BuildAssetBundles("Assets/ABs", BuildAssetBundleOptions.None, BuildTarget.Android);
+        AssetDatabase.Refresh();
+    }
+
+    [MenuItem("Example/Build Win64")]
+    static void BuildABs()
+    {
+        string outputPath = Application.dataPath + "/ABs";
+        if (!Directory.Exists(outputPath))
+        {
+            Directory.CreateDirectory(outputPath);
+        }
+        // Put the bundles in a folder called "ABs" within the Assets folder.
+        BuildPipeline.BuildAssetBundles("Assets/ABs", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+        AssetDatabase.Refresh();
+    }
+
+    [MenuItem("Example/Clean Cache")]
+    static void CleanCache()
+    {
+        Caching.CleanCache();
+    }
+}
